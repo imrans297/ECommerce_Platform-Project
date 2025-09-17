d681e4111dee5ecafbeab6cffe850945d0e5650d
Imran Shaikh <imran.shaikh@einfochips.com>
2025-09-17 15:38:10 +0530

    updated the file again

--- applications/order-service/src/test/java/com/ecommerce/OrderServiceApplicationTest.java
+++ applications/order-service/src/test/java/com/ecommerce/OrderServiceApplicationTest.java
@@ -3,14 +3,15 @@ package com.ecommerce;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureTestMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringBootTest
-@AutoConfigureTestMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureWebMvc
 class OrderServiceApplicationTest {
 
     @Autowired
